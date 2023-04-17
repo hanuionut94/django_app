87fdfe2f04fa52a3d8ea3a06e65ff903085242b9
Hanu Ionut <i_hanu@yahoo.com>
2023-04-17 15:14:33 +0300

    registration added

--- app/members/views.py
+++ app/members/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterUserForm
 
 def login_user(request):
 	if request.method == "POST":
@@ -22,3 +23,17 @@ def logout_user(request):
 	messages.success(request, ('You have been logout!'))
 	return redirect('home')
 
+def register_user(request):
+	if request.method == 'POST':
+		form = RegisterUserForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, ('Registration successful!'))
+			return redirect('home')
+	else:
+		form = RegisterUserForm()
+	return render(request, 'authenticate/register_user.html', {'form':form,})
