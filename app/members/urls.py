9d823447435cfce41721c0b0cb42463d462c2496
Hanu Ionut <i_hanu@yahoo.com>
2023-04-17 22:23:31 +0300

    reset password added

--- app/members/urls.py
+++ app/members/urls.py
@@ -1,10 +1,24 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login_user', views.login_user, name='login'),
     path('logout_user', views.logout_user, name='logout'),
-    path('register_user', views.register_user, name='register_user')
+    path('register_user', views.register_user, name='register_user'),
+
+    path('reset_password/',
+         auth_views.PasswordResetView.as_view(template_name='authenticate/password_reset.html'),
+          name='reset_password'),
+    path('reset_password_sent/', 
+         auth_views.PasswordResetDoneView.as_view(template_name='authenticate/password_reset_sent.htnl'),
+         name='reset_password_done'),
+    path('reset/<uidb64>/<token>', 
+         auth_views.PasswordResetConfirmView.as_view(template_name='authenticate/password_reset_form.html'), 
+         name='password_reset_confirm'),
+    path('reset_password_complete', 
+         auth_views.PasswordResetCompleteView.as_view(template_name='authenticate/password_reset_done.html'), 
+         name='password_reset_complete')
 
 ]
 
