a0b465b0e8b7a7c2730ae127f63fd18e9de2c9e4
Hanu Ionut <i_hanu@yahoo.com>
2023-04-30 11:04:07 +0300

    added cnp field

--- app/bankapp/models.py
+++ app/bankapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    cnp = models.CharField(primary_key=True, max_length=13, unique=True, verbose_name='CNP')
 
 # Create your models here.
--- app/members/forms.py
+++ app/members/forms.py
@@ -1,19 +1,27 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 from django import forms
+from django.core.validators import RegexValidator
+# from .models import User
+from bankapp.models import User
 
 class RegisterUserForm(UserCreationForm):
+    cnp = forms.CharField( min_length=13, max_length=13,
+                          validators=[RegexValidator(r'^[1-9]\d{2}(0[1-9]|1[0-2])(0[1-9]|[12]\d|3[01])(0[1-9]|[1-4]\d|5[0-2]|99)(00[1-9]|0[1-9]\d|[1-9]\d\d)\d$')],
+                          widget=forms.TextInput(attrs={'class':'form-control'}))
+    username = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
     first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
     last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
 
+
     class Meta:
         model = User
-        fields = ('username', 'email','first_name','last_name','password1','password2')
+        fields = ('cnp','username', 'email','first_name','last_name','password1','password2')
 
     def __init__(self, *args, **kwargs):
         super(RegisterUserForm, self).__init__(*args, **kwargs)
-
-        self.fields['username'].widget.attrs['class']='form-control'
+  
+        # self.fields['username'].widget.attrs['class']='form-control'
         self.fields['password1'].widget.attrs['class']='form-control'
         self.fields['password2'].widget.attrs['class']='form-control'
